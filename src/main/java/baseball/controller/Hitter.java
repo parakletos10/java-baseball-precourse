f84a743ed59299d570a6c4524b3cb7b4685fc7a3
parakletos10 <parakletos10@gmail.com>
2022-10-03 17:02:12 +0900

    feat(Hitter) : 게임 플레이어로부터 값을 입력받는 타자 Controller 추가

---
+++ src/main/java/baseball/controller/Hitter.java
@@ -0,0 +1,43 @@
+package baseball.controller;
+
+import baseball.view.HitterInput;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Hitter implements Rule {
+    private Set<Integer> answerValue;
+    private String inputValue;
+
+    public Hitter() {
+        setAnswerValue(new LinkedHashSet<>());
+    }
+
+    public void swing() {
+        setInputValue(new HitterInput().inputValue());
+    }
+
+    public void setAnswerValue() {
+        getAnswerValue().clear();
+
+        for (char character : getInputValue().toCharArray()) {
+            getAnswerValue().add(Integer.parseInt(String.valueOf(character)));
+        }
+    }
+
+    public Set<Integer> getAnswerValue() {
+        return answerValue;
+    }
+
+    public void setAnswerValue(Set<Integer> answerValue) {
+        this.answerValue = answerValue;
+    }
+
+    public String getInputValue() {
+        return inputValue;
+    }
+
+    public void setInputValue(String inputValue) {
+        this.inputValue = inputValue;
+    }
+}
