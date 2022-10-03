843449f4c3c43d1839337e66594bdca4c40f1d68
parakletos10 <parakletos10@gmail.com>
2022-10-03 16:56:42 +0900

    feat(Application) : Application 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.Referee;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Referee referee = new Referee();
+        while (referee.playBall());
     }
 }
